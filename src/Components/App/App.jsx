dfc75d9dce453efd0394c715a5c862ea8b0513ee
Reyner Veliz <reynerv@yahoo.com>
2022-12-02 09:16:37 -0600

    Create App Class and add basic content

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className='highlight'>mmm</span>ing</h1>
+        <div className='App'>
+          {/* Add a SearchBar Component */}
+          <div className='App-playlist'>
+            {/* Add a SearchResults Component */}
+            {/* Add a Playlist Component */}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
